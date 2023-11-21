f359ab72e3b2bcbaca548f10c23a634bcd5fe649
p2592609 <p2592609@GWHXX631WIN017W>
2023-11-21 16:11:59 +0000

    Add OpenGLShader class

---
+++ engine/enginecode/include/independent/platform/OpenGL/OpenGLShader.h
@@ -0,0 +1,28 @@
+/* \file OpenGLShader.h */
+#pragma once
+
+#include <cstdint>
+#include <glm/glm.hpp>
+
+namespace Engine {
+	class OpenGLShader
+	{
+	public:
+		OpenGLShader(const char* vertexFilePath, const char* fragmentFilePath);
+		OpenGLShader(const char* filePath);
+		~OpenGLShader();
+		uint32_t getID() const { return m_OpenGL_ID;  }
+
+		void uploadInt(const char* name, int value);
+		void uploadFloat(const char* name, float value);
+		void uploadFloat2(const char* name, const glm::vec2& value);
+		void uploadFloat3(const char* name, const glm::vec3& value);
+		void uploadFloat4(const char* name, const glm::vec4& value);
+		void uploadMat4(const char* name, const glm::mat4& value);
+
+
+	private:
+		uint32_t m_OpenGL_ID;
+		void compileAndLink(const char* vertexShaderSrc, const char* fragmentShaderSrc);
+	};
+}
